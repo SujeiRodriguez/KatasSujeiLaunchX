c994e6400b476e912bb603a7d9ffc6043f495f07
Sujei Rodriguez <99167736+SujeiRodriguez@users.noreply.github.com>
2022-02-18 22:30:45 -0600

    Completo
    Trate de agregarlas por git bash, pero no me permitio en el ultimo paso

---
+++ Modulo 10/open.py
@@ -0,0 +1,8 @@
+try:
+    open("config.txt")
+except OSError as err:
+    if err.errno == 2:
+        print("Couldn't find the config.txt file!")
+    elif err.errno == 13:
+        print("Found config.txt but couldn't read it")
+Couldn't find the config.txt file!
